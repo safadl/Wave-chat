0a864d1e4ec178adf060c7464c7b8298e0a10187
safad <safadeldoul@gmail.com>
2022-11-07 01:27:51 +0100

    create app

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+import { ExpoConfig } from '@expo/config-types';
+
+// In SDK 47 and higher, use the following import instead:
+// import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+  name: 'my-app',
+};
+
+export default config;
