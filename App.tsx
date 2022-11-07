a0d1b9f85a85f6b3cbf660b1c3db6cca154b3ce6
safad <safadeldoul@gmail.com>
2022-11-07 03:01:57 +0100

    Add splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,72 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, Text, View,Button,Image, Dimensions } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+      
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+  
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.animationContainer}onLayout={onLayoutRootView} >
+      <View        style={{alignSelf:'center',alignItems:'center',justifyContent:'center'}}
+>
+
+      <View style={styles.buttonContainer}>
+      </View>
+      </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  animationContainer: {
+    backgroundColor: '#20232B',
     alignItems: 'center',
     justifyContent: 'center',
+    flex: 1,
   },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+  appName:{
+    color:'white',
+    fontSize:50,
+    letterSpacing:4
+    
+  
+  }
 });
--- app.config.ts
+++ app.config.ts
@@ -5,6 +5,19 @@ import { ExpoConfig } from '@expo/config-types';
 
 const config: ExpoConfig = {
   name: 'my-app',
+  slug: "wave-chat",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    "image": "./assets/fox-hello.png",
+    "resizeMode": "contain",
+    "backgroundColor": "#20232B"
+  
+  },
+  
+  
 };
 
 export default config;
